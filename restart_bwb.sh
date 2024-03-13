4727eec711c172db3bff6016091ee1385cd2d018
Thomas Dahlstrom <tdahlstrom@gmail.com>
2024-03-13 15:10:00 -0700

    Update restart_bwb.sh

--- restart_bwb.sh
+++ restart_bwb.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-docker run --rm -p 6080:6080 -v /workspace/GDC-mRNA-multi-dr32chr22/:/data -v /var/run/docker.sock:/var/run/docker.sock -v /tmp/.X11-unix:/tmp/.X11-unix --privileged --group-add root biodepot/bwb
+docker run --rm -p 6080:6080 -v /workspace/GDC-mRNA-multi-dr32chr22/:/data -v /var/run/docker.sock:/var/run/docker.sock -v /tmp/.X11-unix:/tmp/.X11-unix --privileged --group-add root -e STARTING_WORKFLOW=/data/workflow/GDC_mRNA_multi_dr32chr22/GDC_mRNA_multi_dr32chr22.ows biodepot/bwb
